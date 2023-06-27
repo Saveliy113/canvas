284c3019972b424032ae9a304cef28608fbd2969
Saveliy <sava2698@gmail.com>
2023-06-27 16:06:07 +0600

    Plot added

---
+++ index.js
@@ -0,0 +1,34 @@
+const canvasPlot = document.getElementById('canvas_plot');
+const ctx = canvasPlot.getContext('2d');
+
+const canvasPlotWidth = canvasPlot.clientWidth;
+const canvasPlotHeight = canvasPlot.clientHeight;
+
+const scaleX = 50;
+const scaleY = 50;
+
+// Рисуем сетку
+
+//X-axis
+ctx.beginPath();
+ctx.strokeStyle = '#f5f0e8';
+
+for (let i = 0; i <= canvasPlotWidth; i = i + scaleX) {
+  ctx.moveTo(i, 0);
+  ctx.lineTo(i, canvasPlotHeight);
+}
+
+for (let i = 0; i <= canvasPlotHeight; i = i + scaleY) {
+  ctx.moveTo(0, i);
+  ctx.lineTo(canvasPlotWidth, i);
+}
+
+ctx.stroke();
+ctx.closePath();
+
+//Axises
+const xAxis = canvasPlotWidth / 2;
+const yAxis = canvasPlotHeight / 2;
+
+ctx.beginPath();
+ctx.strokeStyle
