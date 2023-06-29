ef72e7bbb1d307bed1355915ca5beaaaf16e15ef
Saveliy113 <sava2698@gmail.com>
2023-06-29 22:39:13 +0600

    Finished

--- index.js
+++ index.js
@@ -4,31 +4,57 @@ const ctx = canvasPlot.getContext('2d');
 const canvasPlotWidth = canvasPlot.clientWidth;
 const canvasPlotHeight = canvasPlot.clientHeight;
 
-const scaleX = 50;
-const scaleY = 50;
+const scaleX = 30;
+const scaleY = 30;
+
+const xAxis = Math.round(canvasPlotWidth / scaleX / 2) * scaleX;
+const yAxis = Math.round(canvasPlotHeight / scaleY / 2) * scaleY;
+const shiftNumbers = 5;
+const shiftAxis = 10;
+
+ctx.font = `${scaleX / 2}px Arial`;
+ctx.textAlign = 'left';
+ctx.Baseline = 'top';
 
 // Рисуем сетку
-
 //X-axis
 ctx.beginPath();
 ctx.strokeStyle = '#f5f0e8';
 
+ctx.moveTo(xAxis, 0);
+ctx.lineTo(xAxis, canvasPlotHeight);
+ctx.fillText('y', xAxis - shiftAxis * 2, shiftAxis * 2);
+
+ctx.moveTo(0, yAxis);
+ctx.lineTo(canvasPlotWidth, yAxis);
+ctx.fillText('x', canvasPlotWidth - shiftAxis * 2, yAxis - shiftAxis);
+
 for (let i = 0; i <= canvasPlotWidth; i = i + scaleX) {
   ctx.moveTo(i, 0);
   ctx.lineTo(i, canvasPlotHeight);
+
+  ctx.fillText(
+    (i - xAxis) / scaleX,
+    i + shiftNumbers,
+    yAxis + shiftNumbers * 3
+  );
 }
 
 for (let i = 0; i <= canvasPlotHeight; i = i + scaleY) {
   ctx.moveTo(0, i);
   ctx.lineTo(canvasPlotWidth, i);
+
+  ctx.fillText(
+    (yAxis - i) / scaleY,
+    xAxis + shiftNumbers,
+    i + shiftNumbers * 3
+  );
 }
 
 ctx.stroke();
 ctx.closePath();
 
 //Axises
-const xAxis = canvasPlotWidth / 2;
-const yAxis = canvasPlotHeight / 2;
 
 ctx.beginPath();
 ctx.strokeStyle = '#000000';
@@ -41,3 +67,11 @@ ctx.lineTo(canvasPlotWidth, yAxis);
 
 ctx.stroke();
 ctx.closePath();
+
+//Graphic y = x^2
+ctx.fillStyle = '#ff0000';
+for (let i = 0; i <= canvasPlotWidth; i++) {
+  const x = (i - xAxis) / scaleX;
+  const y = Math.pow(x, 2);
+  ctx.fillRect(x * scaleX + xAxis, yAxis - scaleY * y, 4, 4);
+}
